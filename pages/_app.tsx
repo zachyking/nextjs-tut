5e52678052f13240a107431050bde2e33d4d39c8
Dominik Zachar <dominik.zachar@gmail.com>
2021-05-07 02:25:38 +0200

    added chakra UI, implemented darkmode switch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/global.css'
 import { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+        <ChakraProvider>
+            <Component {...pageProps} />
+        </ChakraProvider>
+        </>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 import { GetStaticProps } from 'next'
+import { Text, Heading, Flex, Stack } from '@chakra-ui/react'
+import DarkModeSwitch from '../components/darkmodeswtich'
 
 export default function Home({
   allPostsData
@@ -16,34 +18,49 @@ export default function Home({
   }[]
 }) {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Plain Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+    <Stack 
+    as="main"
+    align="center" 
+    >
+      <Flex 
+        flexDirection="column"
+        maxWidth="1400px" 
+      >
+        <Flex
+          direction="row"
+          w="1400px"
+          pt={4}
+        >
+          <DarkModeSwitch/>
+        </Flex> 
+        <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section className={utilStyles.headingMd}>
+          <p>[Plain Self Introduction]</p>
+          <br/>
+        </section>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </Layout> 
+      </Flex>
+      
+    </Stack>
   )
 }
 
