9584573615d7f1cd7a7e546878d10e3c495e0c17
Dominik Zachar <dominik.zachar@gmail.com>
2021-06-08 02:51:39 +0200

    💄 add explore view on landing

---
+++ components/Explore.tsx
@@ -0,0 +1,161 @@
+import React, { MouseEventHandler } from "react"
+import { Flex, Text, Spacer, IconButton, Button, Heading, useColorMode } from "@chakra-ui/react"
+import {  Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverHeader } from "@chakra-ui/react"
+import { InfoIcon, QuestionIcon } from "@chakra-ui/icons"
+import Logo from "./Logo"
+import NextChakraLink from "./NextChakraLink"
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import utilStyles from '../styles/utils.module.css'
+
+
+
+export default function Explore({ goBackFunc } : { goBackFunc : Function }) {
+    const { colorMode } = useColorMode()
+    const isDark = colorMode === 'dark'
+
+    return (
+        <>
+            <Flex w="100%" direction={{ base: "column", md: "row" }}>
+                
+                <Flex 
+                    direction="column" 
+                    w={["80vw", "80vw", "46vw", "46vw", "45vw", "45vw"]} 
+                    fontSize={["0.625rem", "0.75rem", "0.8rem", "0.9rem", "1.25rem", "1.75rem"]} 
+                    textAlign="left" 
+                    minH={["20vh", "20vh", "50vh"]}
+                    mb={["0vh", "0vh", "5vh"]}
+                    mt={["10vh", "10vh", "unset"]}
+                >
+                  <Flex>
+                    <Text>
+                      WE ARE BRINGING AUDIO NFTS CREATED BY YOU / YOUR TRANSACTION
+                    </Text>
+                  </Flex>
+                  <Spacer/>
+                  <Text>
+                    WHEN WE RECEIVE TRANSACTION, MUSIC CLIP IS ALGORITHMICALLY PUT TOGETHER WITH OUR ORIGINAL SOUNDS, MINTED AND SENT TO YOU
+                  </Text>
+                  <Spacer/>
+                  <Text>
+                    WHICH MAKES EACH TRACK UNIQUE AND YOURS
+                  </Text>
+                  <Spacer/> 
+                </Flex>
+                {/*<Spacer/>*/}
+                <Flex direction="column" w={{ base: "80vw", md: "20vw" }} textColor="gray.600">
+                  <Popover>
+                    <PopoverTrigger>
+                      <IconButton variant="ghost" 
+                        className={utilStyles.shadow}
+                        width={["70vw", "70vw", "25vw", "25vw", "25vw", "25vw" ]}
+                        height={["8vh", "7vh", "15vh", "15vh", "15vh", "15vh"]} 
+                        size="lg" 
+                        aria-label="Sale purpouse" 
+                        pos={[ "inherit", "inherit", "absolute" ]}
+                        left={["2vw", "2vw", "12vw", "12vw", "12vw", "12vw"]}
+                        bottom={["10vh", "10vh", "15vh", "15vh", "15vh", "15vh"]}
+                        color="gray.600"
+                        icon={<InfoIcon />}/>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                      <PopoverArrow />
+                      <PopoverCloseButton />
+                        <PopoverHeader><Logo size="2em" color={ isDark ? "gray.50" : "gray.900" }/></PopoverHeader>
+                      <PopoverBody><Text fontSize="1rem"> By getting our NFT you are supporting development of Cardano Sounds platform</Text></PopoverBody>
+                    </PopoverContent>
+                  </Popover>
+                  <NextChakraLink href="/">
+                    <Button 
+                      width={["70vw", "70vw", "25vw", "25vw", "25vw", "25vw" ]}
+                      mt={["1vh", "1vh", "5vh"]}
+                      height={["8vh", "7vh", "15vh", "15vh", "15vh", "15vh"]} 
+                      variant="ghost"
+                      className={utilStyles.shadow}
+                      position= {["inherit", "inherit", "absolute"]}
+                      right={["20vw", "20vw", "15vw", "15vw", "15vw", "15vw"]}
+                      bottom={["43vh", "43vh", "63vh", "63vh", "63vh", "63vh"]}
+                      transition="all 0.3s ease-in-out"
+                    >
+                      {/* chakra color var doesn't work here */}
+                      <FaChevronRight fill="#4A5568" />
+                      <Heading as="h4" fontSize="1.5rem" textColor="gray.600"
+                        fontWeight="normal"
+                      >
+                        BUY
+                      </Heading>  
+                    </Button>
+                  </NextChakraLink>
+                  <Spacer/>
+                  <NextChakraLink href="/">
+                    <Button 
+                      width={["70vw", "70vw", "25vw", "25vw", "25vw", "25vw" ]}
+                      mt={["1vh", "1vh", "5vh"]}
+                      height={["8vh", "7vh", "15vh", "15vh", "15vh", "15vh"]} 
+                      variant="ghost"
+                      className={utilStyles.shadow}
+                      position= {["inherit", "inherit", "absolute"]}
+                      right={["20vw", "20vw", "15vw", "15vw", "15vw", "15vw"]}
+                      bottom={["32vh", "32vh", "47vh", "47vh", "47vh", "47vh"]}
+                      transition="all 0.3s ease-in-out"
+                    >
+                      {/* chakra color var doesn't work here */}
+                      <FaChevronRight fill="#4A5568" />
+                      <Heading as="h4" fontSize="1.5rem" textColor="gray.600"
+                        fontWeight="normal"
+                      >
+                        ABOUT
+                      </Heading>  
+                    </Button>
+                  </NextChakraLink>
+                  <Spacer/>
+                  <NextChakraLink href="/">
+                    <Button 
+                      width={["70vw", "70vw", "25vw", "25vw", "25vw", "25vw" ]}
+                      mt={["1vh", "1vh", "5vh"]}
+                      height={["8vh", "7vh", "15vh", "15vh", "15vh", "15vh"]} 
+                      variant="ghost"
+                      className={utilStyles.shadow}
+                      position= {["inherit", "inherit", "absolute"]}
+                      right={["20vw", "20vw", "15vw", "15vw", "15vw", "15vw"]}
+                      bottom={["21vh", "21vh", "31vh", "31vh", "31vh", "31vh"]}
+                      transition="all 0.3s ease-in-out"
+                    >
+                      {/* chakra color var doesn't work here */}
+                      <FaChevronRight fill="#4A5568"/>
+                        <Heading fontSize="1.5rem" as="h4" textColor="gray.600"
+                            fontWeight="normal"
+                        >
+                            NFTS
+                        </Heading>  
+                    </Button>
+                  </NextChakraLink>
+                  <Spacer/>
+                  <NextChakraLink href="/">
+                    <Button 
+                      width={["70vw", "70vw", "25vw", "25vw", "25vw", "25vw" ]}
+                      mt={["1vh", "1vh", "5vh"]}
+                      height={["8vh", "7vh", "15vh", "15vh", "15vh", "15vh"]} 
+                      variant="ghost"
+                      className={utilStyles.shadow}
+                      position= {["inherit", "inherit", "absolute"]}
+                      right={["20vw", "20vw", "15vw", "15vw", "15vw", "15vw"]}
+                      bottom={["21vh", "21vh", "31vh", "31vh", "31vh", "31vh"]}
+                      transition="all 0.3s ease-in-out"
+                      display={["flex", "flex", "none"]}
+                      onClick={ goBackFunc as MouseEventHandler<HTMLButtonElement>}
+                    >
+                      {/* chakra color var doesn't work here */}
+                      <FaChevronLeft fill="#4A5568"/>
+                        <Heading fontSize="1.5rem" as="h4" textColor="gray.600"
+                            fontWeight="normal"
+                        >
+                            BACK
+                        </Heading>  
+                    </Button>
+                  </NextChakraLink>
+                  <Spacer />
+                </Flex>
+            </Flex>
+        </>
+    )
+}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
 import utilStyles from '../styles/utils.module.css'
 import { FaGithub, FaTwitter, FaMediumM } from 'react-icons/fa';
 import useSound from 'use-sound';
-import { LinkBox, LinkOverlay } from "@chakra-ui/react"
+import { Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger } from "@chakra-ui/react"
 import mainStyles from './layout.module.css'
 import { useState } from 'react'
 import {
@@ -21,6 +21,9 @@ import {
   Spacer
 } from "@chakra-ui/react"
 import Logo from "./Logo";
+import { QuestionIcon } from "@chakra-ui/icons";
+import Explore from "./Explore";
+import VerticalSocialLinks from "./VerticalSocialLinks";
  
 export default function Hero() {
   const { colorMode } = useColorMode()
@@ -32,12 +35,13 @@ export default function Hero() {
   const [ logoplaying, logoplay ]= useState<boolean>(false)
 
 
-  const playSound = (id: string) => {
-
+  const goBackFunc = () => {
+      explore(false)
   }
 
-  const [play, { stop, sound }] = useSound("/sounds/landingSprite.mp3", {
-     onend: function( soundId: any ) {
+
+  const [play, { stop }] = useSound("/sounds/landingSprite.mp3", {
+     onend: function() {
         console.log(this)
         const self = this
         switch(self._sounds[0]._sprite) {
@@ -83,12 +87,12 @@ export default function Hero() {
           // bgColor={isDark ? ("gray.900") : ("gray.50")}
           // justify={{ base: "center", md: "space-around", xl: "space-between" }}
           direction={{ base: "column-reverse", md: "row" }}
-          h={["60vh", "60vh", "60vh","70vh", "75vh", "80vh"]}
+          minH={["60vh", "60vh", "60vh", "70vh", "100vh", "100vh"]}
         >
           <Stack
             w={{ base: "80vw", md: "75vw" }}
             align="left"
-            mt={["0vh", "0vh", "35vh", "35vh", "15vh", "15vh"]}  
+            mt={["0vh", "0vh", "15vh", "15vh", "0vh", "0vh"]}  
                     
           >
              { !exploring ?
@@ -101,7 +105,7 @@ export default function Hero() {
                   fontWeight="normal"
                   lineHeight="1"
                   transition="all 0.3s ease-in-out" 
-                  className={mainStyles.pointerOnHover}
+                  className={utilStyles.pointerOnHover}
                   onClick={ 
                     !h1playing ? 
                       () => {
@@ -124,7 +128,7 @@ export default function Hero() {
                   fontWeight="normal"
                   lineHeight="1"
                   transition="all 0.4s ease-in-out" 
-                  className={mainStyles.pointerOnHover}
+                  className={utilStyles.pointerOnHover}
                   onClick={ 
                   !h2playing ? 
                     () => {
@@ -146,7 +150,7 @@ export default function Hero() {
                 size={ logoplaying ?  [null, null, "11em", "11em", "16em", "19em"] : [null, null, "10em", "10em", "15em", "18em"]}
                 color={isDark ? "gray.50" : "gray.900"}
                 pos="absolute"
-                top={[null, null, "10vh", "20vh", "15vh", "15vh"]} 
+                top={[null, null, "15vh", "15vh", "15vh", "15vh"]} 
                 right={[null, null, "15vw", "18vw", "18vw", "18vw"]}
                 display={["none", "none", "flex", "flex", "flex", "flex"]}
                 onclick={ 
@@ -162,20 +166,25 @@ export default function Hero() {
                     }
                 }
               />
-            </Flex> : <></>}
+            </Flex> 
+            : 
+            <>
+              <Explore goBackFunc={  goBackFunc } />
+            </>
+            }
             <Flex
               direction={["column", "row", "row", "row" ]}
               w="100%"
             >
               <Heading
                 as="h2"
-                fontSize={ h3playing ? [ "6.125rem", "6.25rem", "6.3rem", "7.25rem", "9rem", "12rem" ]: [ "6rem", "6rem", "6rem", "6.75rem", "8rem", "11rem" ]} 
+                fontSize={ h3playing ? [ "6.125rem", "6.25rem", "6.3rem", "7.25rem", "9rem", "12rem" ] : [ "6rem", "6rem", "6rem", "6.75rem", "8rem", "11rem" ]} 
                 textAlign="left"
                 fontWeight="normal"
                 lineHeight="1"
                 display={exploring ? "none" : "initial"}
+                className={utilStyles.pointerOnHover}
                 transition="all 0.5s ease-in-out"
-                className={mainStyles.pointerOnHover}
                 onClick={ 
                   !h3playing ? 
                     () => {
@@ -208,6 +217,7 @@ export default function Hero() {
                 right={["20vw", "20vw", "15vw", "15vw", "15vw", "15vw"]}
                 bottom={["10vh", "10vh", "15vh", "15vh", "15vh", "15vh"]}
                 transition="all 0.3s ease-in-out"
+                display={ exploring ? ["none", "none", "flex"] : "flex"}
               >
                 {!exploring ?
                 <>
@@ -238,7 +248,7 @@ export default function Hero() {
                 : 
                 <>
                   <FaChevronLeft
-                      //mt={{ base: "5vh", md: "4" }}
+                    //mt={{ base: "5vh", md: "4" }}
                     height="40%"
                     aria-label="Explore CardanoSounds"
                     //size="lg"
@@ -264,113 +274,11 @@ export default function Hero() {
                 }
               </Button>
             </Flex>
-          </Stack>          
-        <Stack 
-            w={{ base: "5vw", md: "10vw" }}
-            align="right"
-            pos="absolute"
-            right={["7vw", "4vw", "5vw", "5vw"]}
-            bottom={[ "7vh", "10vh", "10vh", "15vh", "15vh", "15vh" ]}
-            transition="all 0.3s ease-in-out"
-            //mt={{ base: "10vh", md: "15vh" }}
-          >
-            <Flex display={{ base: "none", md: "flex"}} direction="column" h="50vh">
-              <Heading 
-                display={["none", "none", "flex", "flex"]} 
-                size="lg" 
-                as="h3"
-                className={ isDark ? mainStyles.linkLight : mainStyles.link}
-                //writingMode="tb" 
-                onClick={()=>{}}
-              >TWITTER
-              </Heading>
-              <Spacer />
-              <Heading 
-                display={["none", "none", "flex", "flex"]} 
-                size="lg" 
-                as="h3"
-                className={ isDark ? mainStyles.linkLight : mainStyles.link}
-                //writingMode="tb" 
-                onClick={()=>{}}
-              > GITHUB
-              </Heading>
-              <Spacer />
-              <Heading 
-                display={["none", "none", "flex", "flex"]} 
-                size="lg" 
-                as="h3"
-                className={ isDark ? mainStyles.linkLight : mainStyles.link}
-                //writingMode="tb" 
-                onClick={()=>{}}
-              > MEDIUM
-              </Heading>
-            </Flex>
-            <Flex 
-              justify="center"
-              align="center"
-              direction="column"
-              display={{ base: "flex", md: "none"}}
-            >
-                <NextChakraLink href="https://twitter.com/CardanoSounds">
-                  <IconButton
-                    variant="ghost"
-                    mt={2}
-                    mr={2} 
-                    aria-label="Twitter Cardano Sounds"
-                    size="lg"
-                    icon={
-                          <FaTwitter />
-                    }
-                  />
-                </NextChakraLink>
-                <NextChakraLink href="https://github.com/zachyking/CardanoSounds">
-                  <IconButton
-                    variant="ghost"
-                    mt={2}
-                    mr={2} 
-                    aria-label="Git Cardano Sounds"
-                    size="lg"
-                    icon={
-                          <FaGithub />
-                    }
-                  />
-                </NextChakraLink>
-                 <NextChakraLink href="https://cardanosounds.medium.com">
-                  <IconButton
-                    variant="ghost"
-                    mt={2}
-                    mr={2} 
-                    aria-label="Medium Cardano Sounds"
-                    size="lg"
-                    icon={
-                          <FaMediumM />
-                    }
-                  />
-                </NextChakraLink>
-            </Flex>
+          </Stack>   
 
-          </Stack> 
-          
-         {/*  
-            >
-                <Button
-                  borderRadius="8px"
-                  py={4}
-                  lineHeight={0.75}
-                  size="md"
-                  aria-label="Get NFT"
-                >Explore</Button>
-            </NextChakraLink>
-         
-          
-         
-          */}
-         {/* <Box  w={{ base: "80%", sm: "60%", md: "50%" }} mb={{ base: 0, md: 0 }} mt={{ base: 6, md: 0 }}>
-           <Image src="/new-logo.svg" size="100%" rounded="50%" shadow="2xl" /> 
-           <Image src="/new-logo-big.svg" size="100%" />
-          </Box>*/}
+          <VerticalSocialLinks />
 
-        </Flex>
+      </Flex>
 
   )
 }
