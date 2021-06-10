a9503b07f0542ffb290640638cc11b9814923a63
Dominik Zachar <dominik.zachar@gmail.com>
2021-06-10 03:56:39 +0200

    ✨ 🚧 add buy page needs styles for pc)

--- components/PreBuy.tsx
+++ components/PreBuy.tsx
@@ -1,13 +1,12 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, Heading, List, ListIcon, ListItem, Stack, Spacer, Tooltip, Flex } from "@chakra-ui/react"
-import Head from "next/head"
-import Layout from "./layout"
 import utilStyles from "../styles/utils.module.css"
 import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
 import { InfoIcon } from "@chakra-ui/icons";
 import NextChakraLink from "./NextChakraLink";
 
 export default function PreBuy() {
+    const [isTooltipOpen, setIsTooltipOpen] = useState<boolean>(false)
 
     return(
         <>
@@ -16,54 +15,71 @@ export default function PreBuy() {
                 align="center"
                 margin="auto"
                 maxW={["80vw", "80vw", "75vw", "70vw", "75vw", "70vw"]}
-                mt="25vh"
+                mt={["25vh", "25vh", "30vh", "30vh", "25vh", "25vh"]}
             >
                 <Heading
                     as="h1"
                     size="xl"
                     textAlign={["left", "left", "left", "left", "center", "center"]}
                 >
-                    How to get one of the first Cardano Sounds NFTs?
+                    HOW TO GET ONE OF THE FIRST CARDANO SOUNDS NFTS?
                 </Heading>
-                    <List spacing={3} mt="5vh">
+                    <List spacing={3} ml="0" marginInlineEnd="auto" mt="5vh" >
                         <ListItem>
                             <ListIcon as={FaChevronRight} color="green.500" />
                                 Our collection will be released in 3 short waves
                         </ListItem>
+                        <Spacer/>
                         <ListItem>
                             <ListIcon as={FaChevronRight} color="green.500" />
-                                After we test our solution on testnet, we will announce date/time of the first sale <Tooltip label="To not miss out, follow our socials" fontSize="md"><InfoIcon /></Tooltip>
+                                After we test distribution on testnet, we will announce date/time of the first sale
+                                <Tooltip 
+                                    isOpen={isTooltipOpen}
+                                    label="To not miss out, follow our socials" 
+                                    fontSize="md"
+                                >
+                                    <InfoIcon
+                                        ml="2vw"
+                                        onMouseEnter={() => setIsTooltipOpen(true)}
+                                        onMouseLeave={() => setIsTooltipOpen(false)}
+                                    />
+                                </Tooltip>
                         </ListItem>
+                        <Spacer/>
                         <ListItem>
                             <ListIcon as={FaChevronRight} color="green.500" />
                                 When time is set, an address with QR code and price will be displayed here
                         </ListItem>
+                        <Spacer/>
                         <ListItem>
                             <ListIcon as={FaChevronRight} color="green.500" />
                                 Never use exchange wallet to buy NFT
                         </ListItem>
+                        <Spacer/>
                         <ListItem>
                             <ListIcon as={FaChevronRight} color="green.500" />
                                 To buy multiple NFTs, use multiple transactions
                         </ListItem>
+                        <Spacer/>
                         <ListItem>
                             <ListIcon as={FaChevronRight} color="green.500" />
                                You will be able to check live status of the sound generation and minting process by transaction ID
                         </ListItem>
+                        <Spacer/>
                     </List>
-                    <Flex direction={["column", "column", "row", "row"]} w={["100%", "100%", "90%", "80%"]}>
+                    <Flex direction={["column", "column", "row", "row"]} w={["100%", "100%", "100%", "100%", "100%", "90%"]}>
                         <NextChakraLink href="/">
                             <Button 
                               width={["70vw", "70vw", "25vw", "25vw", "25vw", "25vw" ]}
                               mt={["1vh", "1vh", "5vh"]}
-                              height={["8vh", "7vh", "15vh", "15vh", "15vh", "15vh"]} 
+                              height={["8vh", "7vh", "15vh", "15vh", "15vh", "15vh"]}
                               variant="ghost"
                               className={utilStyles.shadow}
                               transition="all 0.3s ease-in-out"
                             >
                               {/* chakra color var doesn't work here */}
                               <FaChevronLeft fill="#4A5568" />
-                              <Heading as="h4" fontSize="1.5rem" textColor="gray.600"
+                              <Heading className={utilStyles.noHovDecor} as="h4" fontSize="1.5rem" textColor="gray.600"
                                 fontWeight="normal"
                               >
                                 HOME
@@ -71,21 +87,21 @@ export default function PreBuy() {
                             </Button>
                           </NextChakraLink>
                           <Spacer/>
-                          <NextChakraLink href="/">
+                          <NextChakraLink href="/buy">
                             <Button 
                               width={["70vw", "70vw", "25vw", "25vw", "25vw", "25vw" ]}
                               mt={["1vh", "1vh", "5vh"]}
-                              height={["8vh", "7vh", "15vh", "15vh", "15vh", "15vh"]} 
+                              height={["8vh", "7vh", "15vh", "15vh", "15vh", "15vh"]}
                               variant="ghost"
                               className={utilStyles.shadow}
                               transition="all 0.3s ease-in-out"
                             >
-                              {/* chakra color var doesn't work here */}
-                              <FaChevronRight fill="#4A5568"/>
+                                {/* chakra color var doesn't work here */}
+                                <FaChevronRight fill="#4A5568"/>
                                 <Heading fontSize="1.5rem" as="h4" textColor="gray.600"
                                     fontWeight="normal"
                                 >
-                                    ABOUT
+                                    BUY
                                 </Heading>  
                             </Button>
                         </NextChakraLink>
