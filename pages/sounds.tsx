829d0693ad1dbfd6297a4b8a1b721ed78730be73
Dominik Zachar <dominik.zachar@gmail.com>
2021-06-12 03:55:10 +0200

    🚧 ✨ p5 player

--- pages/sounds.tsx
+++ pages/sounds.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import AudioPlayer from '../components/AudioPlayer'
 import SoundNFT from '../components/SoundNFT'
 import { NFTData } from '../interfaces/interfaces'
+//import p5 from 'p5'
+import dynamic from "next/dynamic";
 
+const P5Comp = dynamic(() => import("../components/p5test"),
+  { ssr: false }
+);
 
 export default function Sounds() {
+    
     return (
         <>
            {/* <SoundNFT soundNFTData="https://filesamples.com/samples/audio/flac/sample3.flac" />
             <AudioPlayer url={soundNFTData.media} />*/}
-        <p>.</p>
+            <P5Comp />
         </>
     )
 }
